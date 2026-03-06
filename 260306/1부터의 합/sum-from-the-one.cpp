8b324e683cde3b1f7e8df3b44a157488f9d2ca1f
gimseunglog <20252996@chosun.ac.kr>
2026-03-06 17:47:42 +0900

    Title: 1부터의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260306/1부터의 합/sum-from-the-one.cpp
+++ 260306/1부터의 합/sum-from-the-one.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main() {
     int N;
+    cin >> N;
     int C = 0;
     for(int i = 1; i <= 100; i++){
         C += i;
         if(C >= N){
             cout << i;
+            break;
         }
     }
     return 0;
