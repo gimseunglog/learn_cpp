ee182c08bcb4367d961aaa0084b235270bd44f0c
gimseunglog <20252996@chosun.ac.kr>
2026-03-08 23:12:37 +0900

    Title: 데이터 교환, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260308/데이터 교환/exchange-data.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    int tmp1, tmp2;
+    tmp1 = b;
+    b = a;
+    tmp2 = c;
+    c = tmp1;
+    a = tmp2;
+    cout << a << endl << b << endl << c;
+    return 0;
+}
