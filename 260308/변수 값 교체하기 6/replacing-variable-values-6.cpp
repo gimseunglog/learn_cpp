082a49b3a0d07a3f13cf2aa29ab05a042f6eee2f
gimseunglog <20252996@chosun.ac.kr>
2026-03-08 23:10:54 +0900

    Title: 변수 값 교체하기 6, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260308/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 2;
+    int b = 5;
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+    cout << a << endl << b;
+    return 0;
+}
