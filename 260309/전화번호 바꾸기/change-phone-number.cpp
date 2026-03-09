df11f13760dc1359c60a9ca992d53cc1b1d7cca9
gimseunglog <20252996@chosun.ac.kr>
2026-03-09 11:38:18 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260309/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    int f;
+    char c;
+    cin >> f >> c >> x >> c >> y;
+    cout << '0' << f << c << y << c << x;
+    return 0;
+}
