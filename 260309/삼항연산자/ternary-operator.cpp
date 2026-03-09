d6f9e048d4d5e3940606ef0421810640e8b6362c
gimseunglog <20252996@chosun.ac.kr>
2026-03-09 12:54:20 +0900

    Title: 삼항연산자, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260309/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s;
+    cin >> s;
+    if(s == 100){
+        cout << "pass";
+    }
+    else{
+        cout << "failure";
+    }
+    return 0;
+}
