bd340ba1154990eeb0d3e921eec208662a6857d7
gimseunglog <20252996@chosun.ac.kr>
2026-03-09 17:27:13 +0900

    Title: 4번의 크기 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260309/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C, D, E;
+    cin >> A;
+    cin >> B >> C >> D >> E;
+    cout << (A > B) << endl;
+    cout << (A > C) << endl;
+    cout << (A > D) << endl;
+    cout << (A > E) << endl;
+    return 0;
+}
