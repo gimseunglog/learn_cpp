7aee8c249405c9239fe9083756b1ecc01bffcb50
gimseunglog <20252996@chosun.ac.kr>
2026-03-09 17:22:02 +0900

    Title: 3 또는 5의 배수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260309/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A;
+    cin >> A;
+    if(A % 3 == 0){
+        cout << "YES\n";
+    }
+    else{
+        cout << "NO\n";
+    }
+    if(A % 5 == 0){
+        cout << "YES\n";
+    }
+    else{
+        cout << "NO\n";
+    }
+    return 0;
+}
