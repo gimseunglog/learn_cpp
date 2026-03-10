34fae6e3663fc80bee2328c68b0929471f122045
gimseunglog <20252996@chosun.ac.kr>
2026-03-10 18:47:58 +0900

    Title: 배수의 개수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260310/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int c = 0;
+    for(int i = 0; i < 10; i++){
+        cin >> a;
+        if(a % 2){
+            c++;
+        }
+    }
+    cout << c;
+    return 0;
+}
