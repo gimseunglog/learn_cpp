fb6a8a4ea2e710f0c6135d1866aae4e4fb41ec5b
gimseunglog <20252996@chosun.ac.kr>
2026-03-11 18:38:14 +0900

    Title: 별표 출력하기 5, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260311/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N - i; j++){
+            for(int k = 0; k < N - i; k++){
+                cout << '*';
+            }
+            cout << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
