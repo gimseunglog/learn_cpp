96ea0e0363ec932832e1a8ab8c7e8cf23601d4c0
gimseunglog <20252996@chosun.ac.kr>
2026-03-12 20:44:43 +0900

    Title: 행 단위로 반복하여 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260312/행 단위로 반복하여 출력/print-in-row.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        for(int j = 1; j < N; j++){
+            cout << j;
+        }
+        cout << endl;''''
+    }
+    return 0;
+}
