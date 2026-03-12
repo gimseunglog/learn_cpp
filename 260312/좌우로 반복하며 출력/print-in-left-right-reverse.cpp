503739e4e694278683a43166dceb58c9ed17ebe4
gimseunglog <20252996@chosun.ac.kr>
2026-03-12 21:17:44 +0900

    Title: 좌우로 반복하며 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260312/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        if(i % 2){
+            for(int j = N; j > 0; j --){
+                cout << j;
+            }
+        }
+        else{
+            for(int j = 1; j <= N; j++){
+                cout << j;
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
