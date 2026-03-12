3d8cd10055eef654964fde2fe25f3621046a2e82
gimseunglog <20252996@chosun.ac.kr>
2026-03-12 14:15:24 +0900

    Title: 별표 출력하기 13, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 260312/별표 출력하기 13/print-start-13.cpp
+++ 260312/별표 출력하기 13/print-start-13.cpp
@@ -7,11 +7,26 @@ int main() {
     for(int i = 0; i < N; i++){
         if(i % 2 == 0){
             for(int j = 0; j < N - i / 2; j++){
-                cout << '*';
+                cout << "* ";
             }
         }
         else{
-
+            for(int j = 0; j < i / 2 + 1; j++){
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+    for(int i = N; i > 0; i--){
+        if(i % 2){
+            for(int j = 0; j < N - i / 2; j++){
+                cout << "* ";
+            }
+        }
+        else{
+            for(int j = 0; j < i / 2; j++){
+                cout << "* ";
+            }
         }
         cout << endl;
     }
