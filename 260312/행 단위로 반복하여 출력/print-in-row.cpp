5e5176a42e11374549ea8389eb7801e8360a30b6
gimseunglog <20252996@chosun.ac.kr>
2026-03-12 20:46:00 +0900

    Title: 행 단위로 반복하여 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260312/행 단위로 반복하여 출력/print-in-row.cpp
+++ 260312/행 단위로 반복하여 출력/print-in-row.cpp
@@ -5,10 +5,10 @@ int main() {
     int N;
     cin >> N;
     for(int i = 0; i < N; i++){
-        for(int j = 1; j < N; j++){
+        for(int j = 1; j <= N; j++){
             cout << j;
         }
-        cout << endl;''''
+        cout << endl;
     }
     return 0;
 }
