5d515deac63f552084e3631a0286c8c60f411dc8
gimseunglog <20252996@chosun.ac.kr>
2026-03-14 12:50:36 +0900

    Title: 특정 수 도달하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260314/특정 수 도달하기/reaching-specific-number.cpp
+++ 260314/특정 수 도달하기/reaching-specific-number.cpp
@@ -4,29 +4,22 @@ using namespace std;
 int main() {
     int a;
     int sum = 0;
-    int cnt;
+    int cnt = 0;
     for(int i = 0; i < 10; i++){
         cin >> a;
         if(a >= 250){
             for(int j = i; j < 10; j++){
                 cin >> a;
             }
-            cnt = i;
             break;
         }
+        cnt++;
         sum += a;
     }
     cout << fixed;
     cout.precision(1);
     double avg;
-    if(cnt){
-        avg = (double)sum / cnt;
-        cout << sum << ' ' << avg;
-    }
-    else{
-        cout << sum << " -nan";
-    }
-    
-    
+    avg = (double)sum / cnt;
+    cout << sum << ' ' << avg; 
     return 0;
 }
