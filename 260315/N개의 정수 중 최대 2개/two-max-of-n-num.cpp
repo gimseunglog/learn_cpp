db2bbd821512ec99337137419c4be1985428b0e4
gimseunglog <20252996@chosun.ac.kr>
2026-03-15 21:40:49 +0900

    Title: N개의 정수 중 최대 2개, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260315/N개의 정수 중 최대 2개/two-max-of-n-num.cpp
+++ 260315/N개의 정수 중 최대 2개/two-max-of-n-num.cpp
@@ -13,7 +13,7 @@ int main() {
 
     for(int i = 0; i < N; i++){
         for(int j = 1; j < N - i; j++){
-            if(A[j] < A[j - 1]){
+            if(A[j] > A[j - 1]){
                 int tmp = A[j];
                 A[j] = A[j - 1];
                 A[j - 1] = tmp;
