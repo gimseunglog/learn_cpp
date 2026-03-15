032d3a18da6ec18a7eee58ad71c26fe378089bc4
gimseunglog <20252996@chosun.ac.kr>
2026-03-15 21:28:38 +0900

    Title: 가장 큰 수 고르기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260315/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10];
+    for(int i = 0; i < 10; i++){
+        cin >> a[i];
+    }
+    int max = a[0];
+    for(int i = 1; i < 10; i++){
+        if(a[i] > a[i - 1]){
+            max = a[i];
+        }
+    }
+    cout << max;
+    return 0;
+}
