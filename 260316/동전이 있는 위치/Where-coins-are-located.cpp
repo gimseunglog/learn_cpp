e91e32cb070ce461c1792aed8f04eb90fe2c9130
gimseunglog <20252996@chosun.ac.kr>
2026-03-16 21:37:21 +0900

    Title: 동전이 있는 위치, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260316/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    int a[9][9] = {};
+    for(int i = 0; i < M; i++){
+        int x, y;
+        cin >> x >> y;
+        a[x - 1][y - 1]++;
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << a[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
