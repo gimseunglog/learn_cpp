9064a18f727f3418e73644620a59aa8928ea30d7
gimseunglog <20252996@chosun.ac.kr>
2026-03-16 13:36:37 +0900

    Title: 두 개의 격자 비교하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260316/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    int a[10][10];
+    int b[10][10];
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cin >> a[i][j];
+        }
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cin >> b[i][j];
+        }
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(a[i][j] == b[i][j]){
+                cout << 0 << ' ';
+            }
+            else{
+                cout << 1 << ' ';
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
