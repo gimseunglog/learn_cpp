36502906a49166c254b49a70f23b65a5e6a22a3e
gimseunglog <20252996@chosun.ac.kr>
2026-03-23 13:34:06 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260323/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void printstar(){
+    for(int i = 0; i < 10; i++){
+        cout << '*';
+    }
+    cout << endl;
+}
+
+int main() {
+    for(int i = 0; i < 5; i++){
+        printstar();
+    }
+    return 0;
+}
