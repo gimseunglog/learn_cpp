dca495db1d0524ab9bcb59454d48dbf9bd454279
gimseunglog <20252996@chosun.ac.kr>
2026-03-23 15:53:30 +0900

    Title: 함수를 이용한 윤년 판별, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260323/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_leap(int y){
+    if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0){
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    int y;
+    cin >> y;
+    if(is_leap){
+        cout << "true";
+    }
+    else{
+        cout << "false";
+    }
+    return 0;
+}
