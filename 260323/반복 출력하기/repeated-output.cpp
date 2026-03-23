5b8f8520809cadc9bc58e0059faf7b8c06f065e6
gimseunglog <20252996@chosun.ac.kr>
2026-03-23 13:35:43 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260323/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printnum(int N){
+    for(int i = 0; i < N; i++){
+        cout << "12345^&*()_" <<  endl;
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+    printnum(N);
+    return 0;
+}
