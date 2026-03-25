792073c309236447e6b4f3d48784ce882a10274f
gimseunglog <20252996@chosun.ac.kr>
2026-03-25 16:59:16 +0900

    Title: Factorial, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260325/Factorial/factorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fac(int n){
+    if(n == 1) 
+        return 1;
+    else{
+        return n * fac(n-1);
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+    cout << fac(N);
+    return 0;
+}
