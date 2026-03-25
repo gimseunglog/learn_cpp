04d3625c611426d44a27ce941492c0a42a0281ae
gimseunglog <20252996@chosun.ac.kr>
2026-03-25 16:40:39 +0900

    Title: 각 자리 숫자의 제곱, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260325/각 자리 숫자의 제곱/square-of-each-digit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int sum_square(int N){
+    if(N == 0)
+        return 0;
+    else{
+        int n = N % 10
+        return n * n + sum_square(N / 10);
+    }
+}
+
+int main() {
+    cin >> N;
+
+    cout << sum_square(N);
+
+    return 0;
+}
