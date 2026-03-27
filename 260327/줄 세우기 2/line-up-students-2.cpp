ed9900489900578c7fba6d2f311e01df99af3507
gimseunglog <20252996@chosun.ac.kr>
2026-03-27 22:34:51 +0900

    Title: 줄 세우기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260327/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class information{
+    public:
+        int height, weight, number;
+        information() { }
+        information(int height, int weight, int number){
+            this->height = height;
+            this->weight = weight;
+            this->number = number;
+        }
+};
+
+bool cmp(information a, information b){
+    if(a.height != b.height)
+        return a.height < b.height;
+    return a.weight > b.weight;
+}
+
+int main() {
+    int n;
+    information info[1000];
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int h, w;   
+        cin >> h >> w;
+        info[i] = information(h, w, i + 1);
+    }
+
+    sort(info, info + n, cmp);
+
+    for(int i = 0; i < n; i++){
+        cout << info[i].height << ' ' << info[i].weight << ' ' << info[i].number << endl;
+    }
+
+
+    return 0;
+}
