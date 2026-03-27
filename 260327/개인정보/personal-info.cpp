cdcc3460038fbf452fa09b291cb0d8c43466606e
gimseunglog <20252996@chosun.ac.kr>
2026-03-27 21:38:47 +0900

    Title: 개인정보, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260327/개인정보/personal-info.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class information{
+    public:
+        string name;
+        int height, weight;
+        information() { }
+        information(string name, int height, int weight){
+            this->name = name;
+            this->height = height;
+            this->weight = weight;
+        }
+};
+
+bool cpn(information a, information b){
+    return a.name < b.name;
+}
+
+bool cph(information a, information b){
+    return a.height < b.height;
+}
+
+int main() {
+    information info[5];
+
+    for(int i = 0; i < 5; i++){
+        string n;
+        int h, w;
+        cin >> n >> h >> w;
+        info[i] = information(n, h, w);
+    }
+
+    sort(info, info + 5, cpn);
+
+    cout << "name" << endl;
+    for(int i = 0; i < 5; i++){
+        cout << info[i].name << ' ' << info[i].height << ' ' << info[i].weight << endl;
+    }
+    cout << endl;
+
+    sort(info, info + 5, cph);
+
+    for(int i = 0; i < 5; i++){
+        cout << info[i].name << ' ' << info[i].height << ' ' << info[i].weight << endl;
+    }
+    return 0;
+}
