fe20e8ba64a1713ece127ee27760d635ecbc78a7
gimseunglog <20252996@chosun.ac.kr>
2026-03-27 22:15:26 +0900

    Title: 정렬된 수 위치 알아내기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260327/정렬된 수 위치 알아내기/indices-of-sorted-array.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class point{
+    public:
+        int num, location;
+        point() { }
+        point(int num, int location){
+            this->num = num;
+            this->location = location;
+        }
+};
+
+bool cmp(point a, point b){
+    if(a.num != b.num)
+        return a.num < b.num;
+    return a.location < b.location;
+}
+
+int main() {
+    int N;
+    point p[1000];
+
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        int n;
+        cin >> n;
+        p[i] = point(n, i + 1);
+    }
+
+    sort(p, p + N, cmp);
+
+    for(int i = 0; i < N; i++){
+        cout << p[i].location << ' ';
+    }
+
+    return 0;
+}
