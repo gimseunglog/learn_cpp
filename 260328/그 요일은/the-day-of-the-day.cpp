eb486d681f01b997f04a67e8dc39b8841ea2d671
gimseunglog <20252996@chosun.ac.kr>
2026-03-28 16:16:26 +0900

    Title: 그 요일은, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 260328/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int m[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+enum days {Mon, Tue, Wed, Thu, Fri, Sat, Sun};
+
+int main() {
+    int m1, d1, m2, d2;
+    int day;
+
+    cin >> m1 >> d1 >> m2 >> d2;
+    cin >> day;
+
+    int dt1 = d1;
+    int dt2 = d2;
+
+    for(int i = 0; i < m1; i++){
+        dt1 += m[i];
+    }
+    for(int i = 0; i < m2; i++){
+        dt2 += m[i];
+    }
+
+    int total = dt2 - dt1;
+    int cnt = 0;
+
+    while(total > 0){
+        if(total % 7 == day){
+            cnt++;
+            total -= 7;
+        }
+    }
+
+    cout << cnt;
+    
+    return 0;
+}
