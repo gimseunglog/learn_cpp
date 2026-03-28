ff3a67e27b9dc635151584d76d0cdab95126e462
gimseunglog <20252996@chosun.ac.kr>
2026-03-28 16:21:19 +0900

    Title: 그 요일은, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260328/그 요일은/the-day-of-the-day.cpp
+++ 260328/그 요일은/the-day-of-the-day.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int m[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-enum days {Mon, Tue, Wed, Thu, Fri, Sat, Sun};
 
 int main() {
     int m1, d1, m2, d2;
@@ -14,23 +13,22 @@ int main() {
     int dt1 = d1;
     int dt2 = d2;
 
-    for(int i = 0; i < m1 - 1; i++){
+    for(int i = 0; i < m1 - 1; i++)
         dt1 += m[i];
-    }
-    for(int i = 0; i < m2 - 1; i++){
+
+    for(int i = 0; i < m2 - 1; i++)
         dt2 += m[i];
-    }
 
     int total = dt2 - dt1;
+
     int cnt = 0;
 
-    for(int i = 0; i <= total; i++){
-        if(i % 7 == day){
+    for(int i = 0; i <= total; i++) {
+        if(i % 7 == day)
             cnt++;
-        }
     }
 
     cout << cnt;
-    
+
     return 0;
 }
