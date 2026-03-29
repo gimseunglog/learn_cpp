34f26983672f8d465f483c6d8349ab488fe3dc2c
gimseunglog <20252996@chosun.ac.kr>
2026-03-29 14:39:02 +0900

    Title: 여러가지 진수변환, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260329/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n, b;
+    int cnt = 0;
+    int arr[10000];
+
+    cin >> n >> b;
+    
+    while(true){
+        if(n < b){
+            arr[cnt++] = n;
+            break;
+        }
+        arr[cnt++] = n % b;
+        n /= b;
+    }
+
+    for(int i = cnt - 1; i >= 0; i--){
+        cout << arr[i];
+    }
+
+    return 0;
+}
