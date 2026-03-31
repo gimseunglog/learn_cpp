71503befb3cfdcce10e8f009c3abf281d9a6dc4c
gimseunglog <20252996@chosun.ac.kr>
2026-03-31 16:55:53 +0900

    Title: 색종이의 총 넓이, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260331/색종이의 총 넓이/the-total-area-of-colored-paper.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[201][201] = {0}; 
+    for(int i = 0; i < n; i++){
+        int x, y;
+        cin >> x >> y;
+        x += 100;
+        y += 100;
+        for(int j = y; j < 8; j++){
+            for(int k = x; k < 8; k++){
+                a[j][k] = 1;
+            }
+        }
+    }
+    int cnt = 0;
+    for(int j = 0; j < 201; j++){
+        for(int k = 0; k < 201; k++){
+            if(a[j][k] == 1){
+                cnt++;
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
