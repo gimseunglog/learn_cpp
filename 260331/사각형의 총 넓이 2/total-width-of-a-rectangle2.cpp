bc26651f173d3ac718a30267c42839141ded7317
gimseunglog <20252996@chosun.ac.kr>
2026-03-31 16:36:32 +0900

    Title: 사각형의 총 넓이 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260331/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[201][201] = {0}; 
+    for(int i = 0; i < n; i++){
+        int x1, x2, y1, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        x1 += 100;
+        x2 += 100;
+        y1 += 100;
+        y2 += 100;
+        for(int j = y1; j < y2; j++){
+            for(int k = x1; k < x2; k++){
+                a[j][k]++;
+            }
+        }
+    }
+    int cnt = 0;
+    for(int j = 0; j < 201; j++){
+        for(int k = 0; k < 201; k++){
+            if(a[j][k] > 0){
+                cnt++;
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
